66ecaabf0709429318d970d230cc30b9c0e8afc1
Thiago Santos <thiagomtd2@gmail.com>
2022-05-27 10:23:22 -0300

    passando produto e quantidade para rota qrcode

--- controllers/qrcode.py
+++ controllers/qrcode.py
@@ -1,4 +1,6 @@
+from crypt import methods
 from flask import render_template
+from flask import request
 
 from server.instance import server
 app = server.app
@@ -6,5 +8,7 @@ app = server.app
 
 @app.route('/qrcode')
 def qrcode():
+    quantidade = request.args.get("quantidade")
+    produto = request.args.get("produto")
     qrcode = server.aqui
-    return render_template('qrcode.html', qrcode=qrcode)
+    return render_template('qrcode.html', qrcode=qrcode, quantidade=quantidade, produto=produto)
