8d6da556e4f14d9e22035f96fbdd4a65d878ee51
Thiago Santos <thiagomtd2@gmail.com>
2022-05-27 12:13:08 -0300

    add motores

--- controllers/qrcode.py
+++ controllers/qrcode.py
@@ -1,6 +1,9 @@
 from crypt import methods
+from time import sleep
 from flask import render_template
 from flask import request
+from gpiozero import Motor
+from time import sleep
 
 from server.instance import server
 app = server.app
@@ -12,3 +15,19 @@ def qrcode():
     produto = request.args.get("produto")
     qrcode = server.aqui
     return render_template('qrcode.html', qrcode=qrcode, quantidade=quantidade, produto=produto)
+
+
+def entregarProduto(produto, quantidade):
+    amendoim = Motor(17, 27)
+    jelly = Motor(22, 23)
+
+    if produto == "Amendoim":
+        amendoim.forward()
+        sleep(quantidade*5)
+        amendoim.stop()
+    if produto == "Jelly":
+        jelly.forward()
+        sleep(quantidade*5)
+        jelly.stop()
+
+    return render_template('selecionar.html')
