415e7892c745ac41682dd7dbca37bd34c75b0e32
CantarzoTDC <25806400+CantarzoTDC@users.noreply.github.com>
2022-06-11 15:27:42 -0300

    add

--- controllers/motores.py
+++ controllers/motores.py
@@ -3,11 +3,19 @@ from time import sleep
 
 
 def motor2(tempo):
-    print(tempo, "motor2")
+    motor2 = Motor(22,23)
+    motor2.backward() 
 
+    sleep(tempo)        
+    motor2.stop()
 
 
 
 
 def motor1(tempo):
-   print(tempo, "motor1")
+    motor1 = Motor(17,27)
+
+    motor1.forward()
+
+    sleep(tempo)        
+    motor1.stop()
